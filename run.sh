0f7fb460d29d3ac1e29a91a5bb6ee1b03505a629
Bruno Binet <bruno.binet@gmail.com>
2017-05-23 12:22:01 +0200

    Rename master.pub docker secret to look for

--- run.sh
+++ run.sh
@@ -16,14 +16,14 @@ if [ -n "${KEY_MINION_PRIV}" ]; then
 fi
 
 # create salt minion keys from docker secrets
-if [ -f /run/secrets/minion.pem ] && [ -f /run/secrets/minion.pub ] && [ -f /run/secrets/minion_master.pub ]
+if [ -f /run/secrets/minion.pem ] && [ -f /run/secrets/minion.pub ] && [ -f /run/secrets/master.pub ]
 then
     echo "=> Overwriting minion public & private key from docker secrets"
     mkdir -p "${SALT_CONFIG}/pki/minion"
     chmod 700 "${SALT_CONFIG}/pki/minion"
     cp /run/secrets/minion.pem "${SALT_CONFIG}/pki/minion/minion.pem"
     cp /run/secrets/minion.pub "${SALT_CONFIG}/pki/minion/minion.pub"
-    cp /run/secrets/minion_master.pub "${SALT_CONFIG}/pki/minion/minion_master.pub"
+    cp /run/secrets/master.pub "${SALT_CONFIG}/pki/minion/minion_master.pub"
 fi
 
 if [ -x "$BEFORE_EXEC_SCRIPT" ]
